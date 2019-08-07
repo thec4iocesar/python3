8b2abc1796f11d5996a609082450e6fd2a869e0e
Lucas Ricciardi de Salles <lucas.salles@4linux.com.br>
2019-08-07 18:48:53 -0300

    Aula 3

--- aula_1/ex_7.py
+++ aula_1/ex_7.py
@@ -1,35 +1,159 @@
 
 import os
 import sys
+import hashlib
+import bson
 
 import flask
+import pymongo
+import jwt
 
+
+client = pymongo.MongoClient()
+db = client.frank
 
 app = flask.Flask(__name__)
 
+def extract(user):
+    user['_id'] = str(user['_id'])
+    del user['password']
+    return user
+
 @app.route('/users/auth', methods=[ 'POST' ])
 def auth_users():
-    return 'auth user'
+    
+    form = flask.request.json
+
+    required_attributes = [ 'email', 'password' ]
+
+    for attr in required_attributes:
+        if attr not in form:
+            return flask.jsonify({
+                'message': 'attribute {} required'.format(attr)
+            }), 400
+    
+    email, password = [ form[key] for key in required_attributes ]
+
+    user = db.users.find_one({ 'email': email })
+
+    if not user:
+        return flask.jsonify({
+            'message': 'user not found'
+        }), 404
+    
+    if user.get('password') == hashlib.sha256(password.encode()).hexdigest():
+
+        user = extract(user)
+
+        user['token'] = jwt.encode(user, 'secret')
+        
+        return flask.jsonify({
+            'user': user
+        }), 200
+
+    return flask.jsonify({
+        'message': 'wrong password'
+    }), 400
 
 @app.route('/users', methods=[ 'GET' ])
 def get_users():
-    return 'get users'
+    
+    params = dict(flask.request.args)
+    
+    for key in params.keys():
+        params[key] = params[key][0]
+        try:
+            params[key] = int(params[key])
+        except ValueError:
+            pass
+
+    users = [ 
+        extract(u) for u in db.users.find(params) 
+    ]
+    
+    return flask.jsonify({
+        'users': users 
+    })
 
 @app.route('/users', methods=[ 'POST' ])
 def post_users():
-    return 'post users'
+
+    form = flask.request.json
+    
+    required_attributes = [ 'email', 'password', 'age' ]
+
+    for attr in required_attributes:
+        if attr not in form:
+            return flask.jsonify({
+                'message': 'attribute {} required'.format(attr)
+            }), 400
+
+    user = db.users.find_one({ 'email': form.get('email') })
+
+    if user:
+        return flask.jsonify({
+            'message': 'email already exists'
+        }), 400
+
+    form['password'] = hashlib.sha256(form['password'].encode()).hexdigest()
+
+    db.users.insert_one(form)
+
+    return flask.jsonify({
+        'message': 'user created'
+    }), 201
 
 @app.route('/users/<userid>', methods=[ 'GET' ])
 def get_user_by_id(userid):
-    return 'get {} users'.format(userid)
+
+    user = db.users.find_one({ '_id': bson.ObjectId(userid) })
+    
+    if not user:
+        return flask.jsonify({
+            'message': 'user not found'
+        }), 404
+
+    return flask.jsonify({
+        'user': extract(user)
+    })
 
 @app.route('/users/<userid>', methods=[ 'PUT', 'PATCH' ])
 def update_user_by_id(userid):
-    return 'update {} users'.format(userid)
+
+    user = db.users.find_one({ '_id': bson.ObjectId(userid) })
+    
+    if not user:
+        return flask.jsonify({
+            'message': 'user not found'
+        }), 404
+    
+    for key in flask.request.json.keys():
+        if key in user:
+            user[key] = flask.request.json[key]
+            if key == 'password':
+                user[key] = hashlib.sha256(user[key].encode()).hexdigest()
+
+    db.users.update({ '_id': bson.ObjectId(userid) }, user)   
+    
+    return flask.jsonify({
+        'user': extract(user)
+    }), 200
 
 @app.route('/users/<userid>', methods=[ 'DELETE' ])
 def delete_user_by_id(userid):
-    return 'delete {} users'.format(userid)
+
+    user = db.users.find_one({ '_id': bson.ObjectId(userid) })
+    
+    if not user:
+        return flask.jsonify({
+            'message': 'user not found'
+        }), 404
+
+    db.users.remove({ '_id': bson.ObjectId(userid) })
+
+    return flask.jsonify({
+        'user': extract(user)
+    }), 200
 
 if __name__ == "__main__":
     
