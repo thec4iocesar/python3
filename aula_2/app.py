39396a19b19fbad9e5b41eb562297a373855b4de
Caio Cesar <caiocesarribeiro@hotmail.com>
2019-08-07 21:13:10 -0300

    updates

--- aula_2/app.py
+++ aula_2/app.py
@@ -1,9 +1,18 @@
+
 import sys 
 import os
 
 import flask
 
 app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    context = {
+        'title': 'Python | Sysadmin'
+    }
+    
+    return flask.render_template('index.html', context=context)
 
 if __name__ == "__main__":
 
